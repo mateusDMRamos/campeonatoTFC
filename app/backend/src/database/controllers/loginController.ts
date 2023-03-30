87e6751029b4872c9ed7e2e77333d7415549c8b2
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2023-03-30 15:08:27 -0300

    feat: iniciado o desenvolvimento do middleware para validação de email e senha

--- app/backend/src/database/controllers/loginController.ts
+++ app/backend/src/database/controllers/loginController.ts
@@ -10,6 +10,6 @@ export default class LoginController {
   async login(req: Request, res: Response): Promise<Response> {
     const { validated, token } = await this.service.login(req.body);
     if (validated) return res.status(200).json({ token });
-    return res.status(400).json({ message: 'Invalid email or password' });
+    return res.status(401).json({ message: 'Invalid email or password' });
   }
 }
--- app/backend/src/database/middlewares/loginValidation.ts
+++ app/backend/src/database/middlewares/loginValidation.ts
@@ -1,10 +1,25 @@
 import { NextFunction, Request, Response } from 'express';
 
+const verifyEmail = (email: string): boolean => {
+  const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (email.match(regex)) return true;
+  return false;
+};
+
+const verifyPassword = (password: string): boolean => password.length > 5;
+
 export default function verifyLogin(req: Request, res: Response, next: NextFunction) {
   const { password, email } = req.body;
   if (!password || !email) {
     return res.status(400).json({
       message: 'All fields must be filled',
+    });
+  }
+  const validEmail = verifyEmail(email);
+  const validPassword = verifyPassword(password);
+  if (!validEmail || !validPassword) {
+    return res.status(401).json({
+      message: 'Invalid email or password',
     });
   }
   return next();
