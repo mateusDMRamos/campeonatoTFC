7334f748713508d8f53f5bdeec1b721164afe21a
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2023-03-30 10:56:09 -0300

    feat: adicionado middleware de validação na rota post /login

--- app/backend/src/routes/loginRoutes.ts
+++ app/backend/src/routes/loginRoutes.ts
@@ -1,12 +1,18 @@
-import { Request, Response, Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 import LoginService from '../database/services/loginService';
 import UserModel from '../database/models/UserModel';
 import LoginController from '../database/controllers/loginController';
+import LoginValidation from '../database/middlewares/loginValidation';
 
 const userRouter = Router();
 const userservice = new LoginService(UserModel);
 const userController = new LoginController(userservice);
+const loginValodation = new LoginValidation();
 
-userRouter.post('/', (req: Request, res: Response) => userController.login(req, res));
+userRouter.post(
+  '/',
+  (req: Request, res: Response, next: NextFunction) => loginValodation.verifyLogin(req, res, next),
+  (req: Request, res: Response) => userController.login(req, res),
+);
 
 export default userRouter;
