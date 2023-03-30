91e92ab53f1f36d3a4ad0c8fd2470278b41eff04
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2023-03-30 14:24:00 -0300

    fix: correção dos testes de integração da rota /login

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -1,9 +1,11 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
+import * as jwt from 'jsonwebtoken';
+import * as bcryptjs from 'bcryptjs';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 import { app } from '../app';
-import token from './mocks/login';
+import { token, loginBody, user } from './mocks/login';
 import UserModel from '../database/models/UserModel';
 chai.use(chaiHttp);
 
@@ -17,8 +19,13 @@ describe('Teste de integração da rota /login', () => {
   });
 
   it('Retorna o token caso o login seja feito corretamente', async function () {
-    sinon.stub(UserModel, 'findOne').resolves();
-    const response = await chai.request(app).post('/login');
+    sinon.stub(UserModel, 'findOne').resolves(user);
+    sinon.stub(bcryptjs, 'compareSync').resolves(true);
+
+    const signMock = sinon.mock(jwt);
+    signMock.expects('sign').returns(token.token);
+
+    const response = await chai.request(app).post('/login').send(loginBody);
     expect(response.status).to.be.equal(OK_STATUS);
     expect(response.body).to.deep.equal(token);
   })
--- app/backend/src/tests/mocks/login.ts
+++ app/backend/src/tests/mocks/login.ts
@@ -1,5 +1,25 @@
+import UserModel from "../../database/models/UserModel"
+
 const token = {
-token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjU0NTI3MTg5fQ.XS_9AA82iNoiVaASi0NtJpqOQ_gHSHhxrpIdigiT-fc'
+token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImVtYWlsIjoiYWRtaW5AYWRtaW4uY29tIn0sImlhdCI6MTY4MDE5MjU4OSwiZXhwIjoxNjgwMjc4OTg5fQ.B5kRLtUj0i37GsCMhecpxOpPP5xa2pO86L47_1aifxk'
 }
 
-export default token;
+const loginBody = {
+  email: 'admin@admin.com',
+  password: 'secret_admin'
+}
+
+const user = {
+  dataValues: {
+    username: 'Admin',
+    role: 'admin',
+    email: 'admin@admin.com',
+    password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+  } 
+} as UserModel;
+
+export {
+  token,
+  loginBody,
+  user,
+}
