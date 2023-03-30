1549659710147830cbc4b8e2e708e6a75ff73728
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2023-03-30 08:45:17 -0300

    feat: criada a model e a migration para a tabela 'users'

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,40 @@
+import { INTEGER, STRING, Model } from 'sequelize';
+import db from '.';
+
+export default class UserModel extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+UserModel.init({
+  id: {
+    type: INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  modelName: 'teams',
+  underscored: true,
+  sequelize: db,
+  timestamps: false,
+});
