23d58de74c5b8fc0966423d9c7be693c233bfa2d
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2023-03-30 11:01:04 -0300

    fix: middleware de validação de login alterado

--- app/backend/src/database/middlewares/loginValidation.ts
+++ app/backend/src/database/middlewares/loginValidation.ts
@@ -1,14 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 
-export default class LoginValidation {
-  // eslint-disable-next-line class-methods-use-this
-  public verifyLogin(req: Request, res: Response, next: NextFunction) {
-    const { password, email } = req.body;
-    if (!password || !email) {
-      return res.status(400).json({
-        message: 'All fields must be filled',
-      });
-    }
-    return next();
+export default function verifyLogin(req: Request, res: Response, next: NextFunction) {
+  const { password, email } = req.body;
+  if (!password || !email) {
+    return res.status(400).json({
+      message: 'All fields must be filled',
+    });
   }
+  return next();
 }
--- app/backend/src/routes/loginRoutes.ts
+++ app/backend/src/routes/loginRoutes.ts
@@ -1,17 +1,16 @@
-import { NextFunction, Request, Response, Router } from 'express';
+import { Request, Response, Router } from 'express';
 import LoginService from '../database/services/loginService';
 import UserModel from '../database/models/UserModel';
 import LoginController from '../database/controllers/loginController';
-import LoginValidation from '../database/middlewares/loginValidation';
+import verifyLogin from '../database/middlewares/loginValidation';
 
 const userRouter = Router();
 const userservice = new LoginService(UserModel);
 const userController = new LoginController(userservice);
-const loginValodation = new LoginValidation();
 
 userRouter.post(
   '/',
-  (req: Request, res: Response, next: NextFunction) => loginValodation.verifyLogin(req, res, next),
+  verifyLogin,
   (req: Request, res: Response) => userController.login(req, res),
 );
 
