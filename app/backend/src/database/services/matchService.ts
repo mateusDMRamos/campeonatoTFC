9f1426b73a03ad83997371aafe52cb52285f421f
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2023-04-03 12:34:49 -0300

    fix: corrigido o findAll do matchService

--- app/backend/src/database/services/matchService.ts
+++ app/backend/src/database/services/matchService.ts
@@ -1,12 +1,18 @@
 import { ModelStatic } from 'sequelize';
 import IMatches from '../interfaces/IMatches';
 import MatchesModel from '../models/MatchesModel';
+import Team from '../models/TeamsModel';
 
 export default class MatchesService {
   constructor(private model:ModelStatic<MatchesModel>) {}
 
   public async getAllMatches(): Promise<IMatches[]> {
-    const rows = await this.model.findAll();
+    const rows = await this.model.findAll({
+      include: [
+        { model: Team, as: 'homeTeam', attributes: ['teamName'] },
+        { model: Team, as: 'awayTeam', attributes: ['teamName'] },
+      ],
+    });
     return rows as IMatches[];
   }
 }
